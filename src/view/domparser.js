bb6788c27c0d68922cd319c80400a4e6e0df2a0f
Abbondanzo <peter@abbondanzo.com>
2017-08-15 14:22:55 -0400

    Updated Parser
    Optimized to handle parsing and translations with ease

--- src/view/domparser.js
+++ src/view/domparser.js
@@ -2,205 +2,17 @@
 
 export default class DomParser {
     /**
-     * 
+     * @constructor
      * @param {HTMLElement} html 
      */
     constructor(html) {
         this.html = html;
     }
 
-    /** DEPRECATED
-     * Returns array of elements that contain given string in innerHTML
-     * @param {String} text 
-     * @returns {Array.HTMLElement}
-     */
-    matchText(text) {
-        let body = this.html;
-        let self = this;
-
-        let predicate = function(element) {
-            self._trimString(element.innerHTML) === text;
-        }
-
-        return this._parseElements(predicate, body);
-    }
-
-    /** DEPRECATED
-     * Returns array of elements that contain given string in innerHTML or attributes
-     * @param {String} text 
-     * @returns {Array.HTMLElement}
-     */
-    matchTranslate(text) {
-        let body = this.html;
-        let self = this;
-
-        let predicate = function(element) {
-            let output = false;
-            // Checks for text in attributes
-            let attributes = element.attributes;
-            if (attributes && attributes.length > 0) {
-                Object.keys(attributes).forEach(function(key) {
-                    let identifiers = ['alt', 'title', 'placeholder'];
-                    if (identifiers.indexOf(attributes[key].name) !== -1) {
-                        output = output || text === attributes[key].value;
-                    }
-                })
-
-            }
-
-            // Checks HTML make-up
-            output = output || self._trimString(element.innerHTML) === text;
-
-            // Checks on siblings/multi-part elements
-            let child = element.firstChild;
-            while (child) {
-                output = output || self._trimString(child.textContent) === text;
-                child = child.nextSibling;
-            }
-
-            return output;
-        }
-        return this._parseElements(predicate, body);
-    }
-
-    /** DEPRECATED
-     * Parses entire HTML document
-     * @param {Function} pred Predicate function
-     * @param {HTMLElement} html 
-     * @returns {Array.HTMLElement}
-     */
-    _parseElements(pred, html) {
-        var queue = [];
-        queue.push(html);
-
-        // Builds a list of elements
-        let output = [];
-
-        while (queue.length > 0) {
-            let currentElement = queue.shift(); // Gets first element
-            let ignorable = ['script', 'noscript', 'style'].indexOf(currentElement.tag) !== -1;
-            if (ignorable) {
-                continue; // Skip past script, noscript, and style elements
-            }
-            if (pred(currentElement)) {
-                output.push(currentElement);
-            }
-            if (currentElement.childNodes) {
-                let children = Array.prototype.slice.call(currentElement.childNodes);
-                queue = queue.concat(children);
-            }
-        }
-        console.log(output);
-        return output;
-    }
-
-    /** DEPRECATED
-     * Replaces attributes and innerHTML of all matching elements to given from value
-     * @param {String} from
-     * @param {String} to
-     */
-    replaceTranslations(from, to) {
-        let elementsToTranslate = this.matchTranslate(from);
-        let addLocalized = process.env.ADD_LOCALIZED ? JSON.parse(process.env.ADD_LOCALIZED) : false;
-        let self = this;
-        elementsToTranslate.forEach(function(elem) {
-            // Checks innerHTML
-            if (self._trimString(elem.innerHTML) === from) {
-                elem.innerHTML = to;
-            }
-            // Checks child siblings
-            if (elem.firstChild) {
-                let child = elem.firstChild;
-                let shouldModify = false;
-                while (child) {
-                    if (self._trimString(child.nodeValue) === from) {
-                        child.nodeValue = child.nodeValue.replace(from, to);
-                    }
-                    child = child.nextSibling;
-                }
-            }
-            // Checks attributes
-            if (elem.attributes && elem.attributes.length > 0) {
-                let attributes = elem.attributes;
-                Object.keys(attributes).forEach(function(key) {
-                    let identifiers = ['alt', 'title', 'placeholder'];
-                    let toChange = identifiers.indexOf(attributes[key].name) !== -1 &&
-                        attributes[key].value === from;
-                    if (toChange) {
-                        attributes[key].value = to;
-                    }
-                })
-
-            }
-            // Localized tag
-            if (addLocalized) {
-                elem.setAttribute('localized', '');
-            }
-        });
-    }
-
-    /**
-     * Handles replacing strings by parsing HTML once
-     * @param {Object} json 
-     */
-    modElements(json) {
-        let iter = document.createNodeIterator(this.html, NodeFilter.SHOW_TEXT);
-        let node = iter.nextNode();
-
-        while (node) {
-            let ignoreable = ['html', 'script', 'noscript', 'style'];
-            if (ignoreable.indexOf(node.parentElement.tagName) === -1) {
-                let trimmedNode = this._trimString(node.nodeValue);
-                for (let key in json) {
-                    if (trimmedNode === key) {
-                        node.nodeValue = node.nodeValue.replace(key, json[key]);
-                    }
-                }
-            }
-            node = iter.nextNode();
-        }
-    }
-
-    /**
-     * Parses single given HTML element, replacing all values with 
-     * @param {Node} element 
-     * @param {String} from 
-     * @param {String} to 
-     */
-    _modify(element, from, to) {
-        // Parse attributes
-        let attributes = element.attributes;
-        if (attributes && attributes.length > 0) {
-            Object.keys(attributes).forEach(function(key) {
-                let identifiers = ['alt', 'title', 'placeholder'];
-                if (identifiers.indexOf(attributes[key].name) !== -1 && attributes[key].value === from) {
-                    attributes[key].value = to;
-                }
-            })
-        }
-
-        // Checks HTML make-up
-        if (this._trimString(element.innerHTML) === from) {
-            element.innerHTML = element.innerHTML.replace(from, to);
-            this._addLocalized(element);
-        }
-
-        // Checks child siblings
-        if (element.firstChild) {
-            let child = element.firstChild;
-            while (child) {
-                if (this._trimString(child.nodeValue) === from) {
-                    child.nodeValue = child.nodeValue.replace(from, to);
-                    this._addLocalized(element);
-                }
-                child = child.nextSibling;
-            }
-        }
-    }
-
     /**
      * Adds 'localized' attribute if given Node is HTMLElement
      * @param {Node} element 
+     * @returns {void} Adds 'localized' attribute to given element if set to true in .env
      */
     _addLocalized(element) {
         // Add 'localized' if set
@@ -211,8 +23,50 @@ export default class DomParser {
     }
 
     /**
+     * Inserts given String into a sorted array
+     * @param {String} string 
+     * @param {Array.String} array Assumes this array is sorted
+     * @returns {Array.String}
+     */
+    _addOrderedString(string, array) {
+        if (array.length === 0) {
+            array.push(string);
+            return array;
+        }
+        for (let index = 0; index < array.length; index++) {
+            if (string.localeCompare(array[index]) === 0) {
+                return array;
+            } else if (string.localeCompare(array[index]) < 0) {
+                array.splice(index, 0, string);
+                return array;
+            }
+        }
+        array.push(string)
+        return array;
+    }
+
+    /**
+     * Iterates over text nodes for this HTML, uses given callback
+     * @param {Function} callback
+     * @returns {void} Mutations handled by given callback
+     */
+    _nodeIterator(callback) {
+        let iter = document.createNodeIterator(this.html, NodeFilter.SHOW_TEXT);
+        let node = iter.nextNode();
+
+        while (node) {
+            let ignoreable = ['html', 'script', 'noscript', 'style'];
+            if (ignoreable.indexOf(node.parentElement.tagName.toLowerCase()) === -1) {
+                callback(node);
+            }
+            node = iter.nextNode();
+        }
+    }
+
+    /**
      * Trim down strings for comparison
      * @param {String} string 
+     * @returns {String} Returns string stripped of extra spaces and lines
      */
     _trimString(string) {
         if (!string) {
@@ -221,5 +75,53 @@ export default class DomParser {
         let text = string.replace(/(\r\n|\n|\r)/gm, '').trim();
         text = text.replace(/\s\s+/g, ' ').replace(/&nbsp;/gi, ''); // Replace longer spaces
         return text;
+    }
+
+    /**
+     * Returns an array of text strings on HTML
+     * @returns {Array.String}
+     */
+    getStrings() {
+        let output = [];
+
+        let self = this;
+        let callback = function(node) {
+            let string = self._trimString(node.nodeValue);
+            if (string) {
+                output = self._addOrderedString(string, output);
+            }
+        }
+        this._nodeIterator(callback);
+
+        console.log(output);
+        return output;
+    }
+
+    /**
+     * Handles replacing strings by parsing HTML once
+     * @param {Object} json
+     * @returns {void} Mutates text nodes and input objects
+     */
+    translateStrings(json) {
+        // All text elements
+        let self = this;
+        let callback = function(node) {
+            let trimmedNode = self._trimString(node.nodeValue);
+            for (let key in json) {
+                if (trimmedNode === key) {
+                    self._addLocalized(node.parentElement);
+                    node.nodeValue = node.nodeValue.replace(key, json[key]);
+                }
+            }
+        }
+        this._nodeIterator(callback);
+
+        // Placeholders
+        let placeholders = this.html.querySelectorAll('input, textarea');
+        placeholders.forEach(function(placeholder) {
+            for (let key in json) {
+                placeholder.placeholder = placeholder.placeholder.replace(key, json[key]);
+            }
+        })
     }
 }
--- src/view/view.js
+++ src/view/view.js
@@ -1,6 +1,5 @@
 'use strict';
 
-import htmlparser from 'htmlparser2';
 import domparser from './domparser';
 
 export default class View {
@@ -27,85 +26,13 @@ export default class View {
      * @returns {Array.String}
      */
     getStrings() {
-        let html = this.html.innerHTML;
-        let self = this;
-
-        let strings = []
-
-        let handler = new htmlparser.DomHandler(function(error, dom) {
-            if (error) {
-                console.warn(error.message);
-            } else {
-                strings = self._processDOM(dom);
-            }
-        })
-        let parser = new htmlparser.Parser(handler);
-        parser.write(html);
-        parser.end();
-
-        console.info('Found ' + strings.length + ' strings:', strings);
-
-        return strings;
-    }
-
-    /**
-     * Inserts given String into a sorted array
-     * @param {String} string 
-     * @param {Array.String} array Assumes this array is sorted
-     * @returns {Array.String}
-     */
-    _addString(string, array) {
-        if (array.length === 0) {
-            array.push(string);
-            return array;
-        }
-        for (let index = 0; index < array.length; index++) {
-            if (string.localeCompare(array[index]) === 0) {
-                return array;
-            } else if (string.localeCompare(array[index]) < 0) {
-                array.splice(index, 0, string);
-                return array;
-            }
-        }
-        array.push(string)
-        return array;
-    }
-
-    /**
-     * Processes JSON data and returns Array of text strings
-     * @param {Array} domJSON Top-level DOM elements in Array format
-     */
-    _processDOM(domJSON) {
-        var strings = []; // "let" causes scope issue within while loop
-
-        var queue = domJSON;
-
-        while (queue.length > 0) {
-            // Get first element of the queue
-            let current = queue.shift();
-
-            // Add any children to the queue for processing
-            let moreContent = current.type !== 'script' && current.type !== 'style' && current.name !== 'noscript';
-            if (moreContent && current.children) {
-                queue = queue.concat(current.children);
-            }
-
-            // Parsing only text items
-            if (current.type === 'text' && current.data) {
-                let text = current.data.replace(/(\r\n|\n|\r)/gm, '').trim();
-                text = text.replace(/\s\s+/g, ' '); // Replace longer spaces
-                let trimmed = text.replace(/&nbsp;/gi, ''); // Removing non-string queries
-                if (text && trimmed) {
-                    strings = this._addString(text, strings);
-                }
-            }
-        }
-
-        return strings;
+        let parser = new domparser(this.html);
+        return parser.getStrings();
     }
 
     /**
      * Replaces the body with a JSON display of all translatable strings
+     * @returns {void} Replaces HTML body with text Node strings found on page
      */
     showJson() {
         let body = this.html;
@@ -117,24 +44,20 @@ export default class View {
         body.innerHTML = '<pre>' + JSON.stringify(jsonObject, null, '\t') + '</pre>';
     }
 
+    /**
+     * Translates strings, measures time to completion
+     * @param {Object} json 
+     * @returns {void} Translates all strings found in given JSON, outputs time to completion in console
+     */
     translateStrings(json) {
         let start = performance.now();
         let parser = new domparser(this.html);
 
         // domparser at work
-        parser.modElements(json);
+        parser.translateStrings(json);
 
         let end = performance.now();
         let time = end && start ? ' in ' + (Math.round(100 * (end - start)) / 100).toString() + ' ms' : '';
         console.info('Translated from ' + Object.keys(json).length + ' strings' + time); // Log time and string count
-    }
-
-    _replaceString(from, to) {
-        let toString = to;
-        let far = findAndReplace(this.html, {
-            preset: 'prose',
-            find: from,
-            replace: to
-        });
     }
 }
